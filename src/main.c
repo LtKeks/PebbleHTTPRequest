ab26684417907ddb5486783e014b4318dee4fbda
LtKeks <LtKeks@users.noreply.github.com>
2015-01-05 18:20:55 +0100

    ZIP for importing to cloudpebble

--- src/main.c
+++ src/main.c
@@ -1,22 +1,18 @@
 #include <pebble.h>
   
   
-/*KEY Werte:
-1-up
-2-long up
-3-select
-4-long select
-5-down
-6-long down
-*/
+
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+
+// Key
 
 enum {
   KEY_MESSAGE = 0,
 };
 
+// AppMessage send function. Send a key with a value to the JS part. 
 
 void send_int(uint8_t key, uint8_t cmd)
 {
@@ -29,6 +25,19 @@ void send_int(uint8_t key, uint8_t cmd)
     app_message_outbox_send();
 }
 
+
+/*
+
+Call the send function with an int value for every button.
+
+1-up
+2-long up
+3-select
+4-long select
+5-down
+6-long down
+
+*/
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   send_int(0, 3);
@@ -64,46 +73,36 @@ static void click_config_provider(void *context) {
 }
 
 static void update_time() {
-  // Get a tm structure
+  
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
-
-  // Create a long-lived buffer
+  
   static char buffer[] = "00:00";
-
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    //Use 2h hour format
+  
+  if(clock_is_24h_style() == true) {    
     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
-    //Use 12 hour format
+  } else {    
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-  }
-
-  // Display this time on the TextLayer
+  }  
   text_layer_set_text(s_time_layer, buffer);
 }
 
 static void main_window_load(Window *window) {
-  // Create time TextLayer
+  
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorBlack);
   text_layer_set_text_color(s_time_layer, GColorClear);
   text_layer_set_text(s_time_layer, "00:00");
-
-  // Improve the layout to be more like a watchface
+  
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
 
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));  
   
-  // Make sure the time is displayed from the start
   update_time();
 }
 
-static void main_window_unload(Window *window) {
-  // Destroy TextLayer
+static void main_window_unload(Window *window) {  
   text_layer_destroy(s_time_layer);
 }
 
@@ -112,29 +111,24 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
   
 static void init() {
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
-  
+ 
+  s_main_window = window_create();  
   window_set_click_config_provider(s_main_window, click_config_provider);
-
-  // Set handlers to manage the elements inside the Window
+  
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
+  
   window_set_fullscreen(s_main_window, true);
   window_set_background_color(s_main_window, GColorBlack);
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum()); 
-
-  // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
-  
-  // Register with TickTimerService
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());   
+  window_stack_push(s_main_window, true);  
+  update_time();  
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
-static void deinit() {
-  // Destroy Window
+static void deinit() {  
   window_destroy(s_main_window);
 }
 
