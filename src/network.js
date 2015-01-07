9e580d49991d0f04aa6f29254ea18b50d8468262
LtKeks <LtKeks@users.noreply.github.com>
2015-01-07 21:29:44 +0100

    Comment/Cleaning

--- src/network.js
+++ src/network.js
@@ -1,36 +1,43 @@
 Pebble.addEventListener("ready", function(e) {});
 
 Pebble.addEventListener("appmessage", function(e) {
-  var xmlHttp = new XMLHttpRequest();
+
+/*
+Case:
+1-up
+2-long up
+3-select
+4-long select
+5-down
+6-long down
+*/
+  
+  
+ var xmlHttp = new XMLHttpRequest();
                 
-switch(e.payload.message) {
+ switch(e.payload.message) {
    
     case 1:        
-        xmlHttp.open( "GET", "http://192.168.178.31/?run=lichtan");        
-        console.log('Case 1');
+        xmlHttp.open( "GET", "http://192.168.178.31/?run=lichtan");
         break;
     case 2:        
         xmlHttp.open( "GET", "http://192.168.178.31/?run=lichtaus");
-        console.log('Case 2');
         break;
     case 3:          
-        xmlHttp.open( "GET", "http://192.168.178.31:8080/jsonrpc?request=%7B%22jsonrpc%22%3A+%222.0%22%2C+%22method%22%3A+%22Player.PlayPause%22%2C+%22params%22%3A+%7B+%22playerid%22%3A+1+%7D%2C+%22id%22%3A+1%7D", false );
-        console.log('Case 3');
+        xmlHttp.open( "GET", "http://192.168.178.31:8080/jsonrpc?request=%7B%22jsonrpc%22%3A+%222.0%22%2C+%22method%22%3A+%22Player.PlayPause%22%2C+%22params%22%3A+%7B+%22playerid%22%3A+1+%7D%2C+%22id%22%3A+1%7D");
         break;
-    case 4:
-        console.log('Case 4');
+    case 4:        
         break;
     case 5:        
-        xmlHttp.open( "GET", "http://192.168.178.31/?run=tvan");        
-        console.log('Case 5');
+        xmlHttp.open( "GET", "http://192.168.178.31/?run=tvan");
         break;
     case 6:        
-        xmlHttp.open( "GET", "http://192.168.178.31/?run=tvaus");        
-        console.log('Case 6');
+        xmlHttp.open( "GET", "http://192.168.178.31/?run=tvaus");
         break;
     default:
-        console.log('Case Default / Error');
-        
-                              } 
-       xmlHttp.send(null);
+        console.log('Case Default / Error');        
+      } 
+  
+ xmlHttp.send(null);
+  
 });
